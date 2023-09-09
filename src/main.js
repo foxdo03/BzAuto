39b818203eaf08383ba6f88f43cbf34f353ace7c
Jun <foxdo03@gmail.com>
2023-09-09 16:26:09 +0900

    JS-Mobile Menu

--- src/main.js
+++ src/main.js
@@ -1,13 +1,45 @@
 // since page is scorlling down, dark mode
 
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
+
 document.addEventListener('scroll', () => {
-  // 스크롤 되는 Y 좌표가 headerHeight 보다 크다면 다른 스타일링
-  console.log(window.scrollY);
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+// home 섹션 아래로 스크롤시 투명화
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// 화살표삭제
+
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY / homeHeight > 0.5) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
+
+// Navbar 토글버튼 클릭 처리
+
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
 });
